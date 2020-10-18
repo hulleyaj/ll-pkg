3874584d35e48075d48944a31d440e7d3657002f
Allen Hulley <hulleyaj@gmail.com>
2020-10-18 10:57:18 -0400

    Buttons

---
+++ components/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button as WebButton } from '@material-ui/core';
+
+const Button = () => (
+  <WebButton variant="contained" color="primary" disableElevation>Web Button</WebButton>
+);
+
+export default Button;
